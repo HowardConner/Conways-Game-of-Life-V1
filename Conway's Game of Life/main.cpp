8a671ef39e521f1c917ae1fa459b620d64c0a158
Conner Howard <conner.howard@wsu.edu>
2023-11-01 09:52:14 -0700

    Began reorginizing event handling with flag system. Only partialy implimented system.

--- Conway's Game of Life/main.cpp
+++ Conway's Game of Life/main.cpp
@@ -11,11 +11,31 @@
 #include "CameraController.hpp"
 #include "BoolBoard.hpp"
 #include "ConwaySolver.hpp"
-
 #include "RectDisplay.hpp"
+#include "Game-Engine/Component Classes/Object_Flags.hpp"
 
 const float movementAmount = 0.10;
 const float zoomAmount = 20.0;
+
+enum class InputEvent
+{
+	MoveUp			= 1 << 0,
+	MoveDown		= 1 << 1,
+	MoveLeft		= 1 << 2,
+	MoveRight		= 1 << 3,
+	ZoomIn			= 1 << 4,
+	ZoomOut			= 1 << 5,
+	CenterOn		= 1 << 6,
+	DragStarted		= 1 << 7,
+	DragActive		= 1 << 8,		// likely redunduant by using "CursorMovment"
+	DragEnded		= 1 << 9,
+	SaveData		= 1 << 10,
+	LoadSaveData	= 1 << 11,
+	CursorMovement	= 1 << 12,
+	//Flag14 = 1 << 13,
+	//Flag15 = 1 << 14,
+	//Flag16 = 1 << 15
+};
 
 struct ProgramBindings {
 	sf::Keyboard::Key moveUp = sf::Keyboard::W;
@@ -36,19 +56,22 @@ int main()
 {
 	// Define Variables
 	string appTitle = "Conway's Game of Life";
+
+	// define program characteristics
 	sf::Vector2f screenDim(1280, 720);
 	ProgramBindings bindings;
 	bool pauseSim = true;
 	int ticksPerSecond = 15;
 	float secondsPerTick = 1.0 / ticksPerSecond;
 
+	// define program resources
 	sf::Clock clock;
 	float deltaTime = 0.0;
 	sf::RenderWindow window(sf::VideoMode(screenDim.x, screenDim.y), appTitle);
 	sf::RectangleShape shape(sf::Vector2f(100, 100));
 	sf::Vector2f simStateScreenPercent(0.1, 0.1);
 	sf::Image tempImage;
-
+	Flag_16<InputEvent> ProgramEvent;
 
 	sf::Texture simActiveTexture;
 	tempImage.create(200, 200, sf::Color::Green);
@@ -125,17 +148,15 @@ int main()
 				}
 				else if (event.key.code == bindings.zoomIn)
 				{
-					camera.zoom(zoomAmount);
+					ProgramEvent.SetFlag(InputEvent::ZoomIn);
 				}
 				else if (event.key.code == bindings.zoomOut)
 				{
-					camera.zoom(-1 * zoomAmount);
+					ProgramEvent.SetFlag(InputEvent::ZoomOut);
 				}
 				else if (event.key.code == bindings.saveKey)
 				{
-					// save the current array for reloading
-					world.saveCurState(bbSave1);
-					std::cout << "save updated" << std::endl;
+					ProgramEvent.SetFlag(InputEvent::SaveData);
 				}
 
 			}
@@ -169,9 +190,7 @@ int main()
 				}
 				else if (event.key.code == sf::Keyboard::R)
 				{
-					// reload save state
-					std::cout << "loading save 1" << std::endl;
-					world.loadCurState(bbSave1);
+					ProgramEvent.SetFlag(InputEvent::LoadSaveData);
 				}
 			}
 			// ===============================================
@@ -179,9 +198,9 @@ int main()
 			// ===============================================
 			if (event.type == sf::Event::MouseButtonPressed)
 			{
-				if (event.mouseButton.button == bindings.dragButton && !camera.isDraggingActive())
+				if (event.mouseButton.button == bindings.dragButton)
 				{
-					camera.startDragEvent(window);
+					ProgramEvent.SetFlag(InputEvent::DragStarted);
 				}
 				// Handle click to add/remove tile type
 				// only activate if not dragging
@@ -208,28 +227,24 @@ int main()
 				if (event.key.code == event.mouseButton.button)
 				{
 					// unbind drag
-					camera.finishDragEvent();
+					ProgramEvent.SetFlag(InputEvent::DragEnded);
 				}
 			}
 			else if (event.type == sf::Event::MouseMoved)
 			{
 				// check if drag is active
-				if (camera.isDraggingActive())
-				{
-					// it is, handle drag
-					camera.handleDragEvent();
-				}
+				ProgramEvent.SetFlag(InputEvent::CursorMovement);
 			}
 			else if (event.type == sf::Event::MouseWheelScrolled)
 			{
 				// check zoom in
 				if (event.mouseWheelScroll.delta > 0)
 				{
-					camera.zoom(zoomAmount);
+					
 				}
 				else if (event.mouseWheelScroll.delta < 0)
 				{
-					camera.zoom(-1 * zoomAmount);
+					
 				}
 			}
 
@@ -241,22 +256,83 @@ int main()
 		// ===============================================
 		if (sf::Keyboard::isKeyPressed(bindings.moveUp))
 		{
-			camera.move(CamDirection::UP, movementAmount);
+			ProgramEvent.SetFlag(InputEvent::MoveUp);
 		}
 		if (sf::Keyboard::isKeyPressed(bindings.moveDown))
 		{
-			camera.move(CamDirection::DOWN, movementAmount);
+			ProgramEvent.SetFlag(InputEvent::MoveDown);
 		}
 		if (sf::Keyboard::isKeyPressed(bindings.moveLeft))
 		{
-			camera.move(CamDirection::LEFT, movementAmount);
+			ProgramEvent.SetFlag(InputEvent::MoveLeft);
 		}
 		if (sf::Keyboard::isKeyPressed(bindings.moveRight))
 		{
-			camera.move(CamDirection::RIGHT, movementAmount);
+			ProgramEvent.SetFlag(InputEvent::MoveRight);
 		}
 
 
+
+
+		// ==============================================================================================
+		//										Process Event Flags
+		// ==============================================================================================
+		if (ProgramEvent.HasFlag(InputEvent::MoveUp))
+		{
+			// Move Main Camera Upwards
+			camera.move(CamDirection::UP, movementAmount);
+		}
+		if (ProgramEvent.HasFlag(InputEvent::MoveDown))
+		{
+			// Move Main Camera Downwards
+			camera.move(CamDirection::DOWN, movementAmount);
+		}
+		if (ProgramEvent.HasFlag(InputEvent::MoveLeft))
+		{
+			// Move Main Camera Left
+			camera.move(CamDirection::LEFT, movementAmount);
+		}
+		if (ProgramEvent.HasFlag(InputEvent::MoveRight))
+		{
+			// Move Main Camera Right 
+			camera.move(CamDirection::RIGHT, movementAmount);
+		}
+		if (ProgramEvent.HasFlag(InputEvent::ZoomIn))
+		{
+			// scroll up, zoom out
+			camera.zoom(zoomAmount);
+		}
+		if (ProgramEvent.HasFlag(InputEvent::ZoomOut))
+		{
+			// scroll down, zoom in
+			camera.zoom( -1 * zoomAmount);
+		}
+		if (ProgramEvent.HasFlag(InputEvent::SaveData))
+		{
+			// save the current array for reloading
+			world.saveCurState(bbSave1);
+			std::cout << "save updated" << std::endl;
+		}
+		if (ProgramEvent.HasFlag(InputEvent::LoadSaveData))
+		{
+			// reload save state
+			std::cout << "loading save 1" << std::endl;
+			world.loadCurState(bbSave1);
+		}
+
+		// drag events
+		if (ProgramEvent.HasFlag(InputEvent::DragStarted) && !camera.isDraggingActive())
+		{
+			camera.startDragEvent(window);
+		}
+		else if (ProgramEvent.HasFlag(InputEvent::DragEnded) && camera.isDraggingActive())
+		{
+			camera.finishDragEvent();
+		}
+		else if (ProgramEvent.HasFlag(InputEvent::CursorMovement) && camera.isDraggingActive())
+		{
+			camera.handleDragEvent();
+		}
 
 
 
